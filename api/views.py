c82bbc979c99cf92085edcc212a7e462eff39aad
Kabir Singh <kabirsingh12b@gmail.com>
2026-02-14 13:09:58 +0530

    feat : successfully returned AI API response on POST req

--- api/views.py
+++ api/views.py
@@ -1,12 +1,32 @@
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from google import genai
+from django.conf import settings
 
 # Create your views here.
+
+
 class GeneratePost(APIView):
-    def get(self,req):
-        return Response('hellow',status=status.HTTP_200_OK)
+    # def get(self,req):
+    #     return Response('hellow',status=status.HTTP_200_OK)
     def post(self,req):
-        fields = req.data.fields
-        return Response(fields,status=status.HTTP_201_CREATED)
+        tone, targetAudience,topic,length = req.data.values()
+        client = genai.Client(api_key=settings.GEMINI_API_KEY)
+        prompt = f"""You are an expert LinkedIn content strategist.
+                    Write a LinkedIn post using the following parameters:
+                        Tone: {tone}
+                        Target Audience: {targetAudience}
+                        Topic: {topic}
+                        Length: {length}
+                    Constraints:
+                        - Start with a strong hook in the first 2 lines.
+                        - Use short paragraphs.
+                        - Speak directly to the target audience.
+                        - Provide actionable insight.
+                        - Avoid generic motivational fluff.
+                        - End with a thought-provoking question."""
+        response = client.models.generate_content(
+        model="gemini-3-flash-preview", contents=prompt)
+        return Response(f'post:{response.text}',status=status.HTTP_201_CREATED)
         
