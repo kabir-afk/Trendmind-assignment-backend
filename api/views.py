df8065a03239935332f31344d9501ebf17444562
Kabir Singh <kabirsingh12b@gmail.com>
2026-02-14 12:07:55 +0530

    chore : added generate view

--- api/views.py
+++ api/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 # Create your views here.
+class GeneratePost(APIView):
+    def get(self,req):
+        return Response('hellow',status=status.HTTP_200_OK)
+    def post(self,req):
+        fields = req.data.fields
+        return Response(fields,status=status.HTTP_201_CREATED)
+        
