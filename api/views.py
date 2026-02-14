c60827b43d45842e7423553d74ff06a819acd83b
Kabir Singh <kabirsingh12b@gmail.com>
2026-02-14 15:48:56 +0530

    chore : added length instruction instead of abstract length
    changed response status code

--- api/views.py
+++ api/views.py
@@ -5,6 +5,12 @@ from google import genai
 from django.conf import settings
 
 # Create your views here.
+
+length_map = {
+    "short": "Keep the post between 100–150 words.",
+    "medium": "Keep the post between 200–300 words.",
+    "long": "Keep the post under 450 words."
+}
 class GeneratePost(APIView):
     def post(self,req):
         tone = req.data.get("tone")
@@ -19,12 +25,14 @@ class GeneratePost(APIView):
             )
 
         client = genai.Client(api_key=settings.GEMINI_API_KEY)
+        length_instruction = length_map.get(length, length_map["short"])
+
         prompt = f"""You are an expert LinkedIn content strategist.
                     Write a LinkedIn post using the following parameters:
                         Tone: {tone}
                         Target Audience: {targetAudience}
                         Topic: {topic}
-                        Length: {length}
+                        Length: {length_instruction}
                     Constraints:
                         - Start with a strong hook in the first 2 lines.
                         - Use short paragraphs.
@@ -34,5 +42,4 @@ class GeneratePost(APIView):
                         - End with a thought-provoking question."""
         response = client.models.generate_content(
         model="gemini-3-flash-preview", contents=prompt)
-        return Response(response.text,status=status.HTTP_201_CREATED)
-        
+        return Response({'post':response.text},status=status.HTTP_200_OK)
